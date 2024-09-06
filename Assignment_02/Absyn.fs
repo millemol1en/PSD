48aae95c75b5ae736c231c892bc66717b9f1e40b
millemol1en <emil.ladegaard1@gmail.com>
2024-09-06 12:24:22 +0200

    Can now use the Lexer and Parser FSharp Library.

--- Assignment_02/Absyn.fs
+++ Assignment_02/Absyn.fs
@@ -5,7 +5,7 @@
 module Absyn
 
 type expr = 
-  | CstI of int
-  | Var of string
-  | Let of string * expr * expr
-  | Prim of string * expr * expr
+    | CstI of int
+    | Var of string
+    | Let of string * expr * expr
+    | Prim of string * expr * expr
