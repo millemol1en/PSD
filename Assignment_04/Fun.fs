01a5c81640c6d04a737a7766a9df75475a429514
millemol1en <emil.ladegaard1@gmail.com>
2024-09-23 13:13:06 +0200

    Pipi und kaka ind pipi kakaland

--- Assignment_04/Fun.fs
+++ Assignment_04/Fun.fs
@@ -13,7 +13,7 @@ open Absyn
 open Parse
 
 (* Environment operations *)
-
+// NOTE: The 'v is a generic type
 type 'v env = (string * 'v) list
 
 let rec lookup env x =
@@ -25,7 +25,7 @@ let rec lookup env x =
 
 type value = 
   | Int of int
-  | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Closure of string * string list * expr * value env       (* (f, x, fBody, fDeclEnv) *)
 
 let rec eval (e : expr) (env : value env) : int =
     match e with 
@@ -56,12 +56,19 @@ let rec eval (e : expr) (env : value env) : int =
     | Letfun(f, x, fBody, letBody) -> 
       let bodyEnv = (f, Closure(f, x, fBody, env)) :: env 
       eval letBody bodyEnv
-    | Call(Var f, eArg) -> 
+    | Call(Var f, eArg) ->
+      // A function call Call(Var f, eArg) is evaluated by first checking that f is bound to a function closure
+      // Closure (f, x, fBody, fDeclEnv). Then the argument expression eArg is evaluated to obtain an argument
+      // value xVal
+      
       let fClosure = lookup env f
       match fClosure with
-      | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = Int(eval eArg env)
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
+      | Closure (f, argLst, fBody, fDeclEnv) ->
+        // 01. We use 'List.zip' to fix together the "eArg" argument expressions 
+        let boundArgs = (List.zip argLst eArg)
+        let argValues = List.map(fun (argName, argExpr) -> (argName, Int(eval argExpr env))) boundArgs
+        // We evaluate the argument 'eArg' within the functions scope, here specified by the "Closure()" type.
+        let fBodyEnv =  argValues @ (f, fClosure) :: fDeclEnv
         eval fBody fBodyEnv
       | _ -> failwith "eval Call: not a function"
     | Call _ -> failwith "eval Call: not first-order function"
@@ -70,51 +77,7 @@ let rec eval (e : expr) (env : value env) : int =
 
 let run e = eval e [];;
 
-(* Examples in abstract syntax *)
-
-let ex1 = Letfun("f1", "x", Prim("+", Var "x", CstI 1), 
-                 Call(Var "f1", CstI 12));;
-
-(* Example: factorial *)
-
-let ex2 = Letfun("fac", "x",
-                 If(Prim("=", Var "x", CstI 0),
-                    CstI 1,
-                    Prim("*", Var "x", 
-                              Call(Var "fac", 
-                                   Prim("-", Var "x", CstI 1)))),
-                 Call(Var "fac", Var "n"));;
-
-(* let fac10 = eval ex2 [("n", Int 10)];; *)
-
-(* Example: deep recursion to check for constant-space tail recursion *)
-
-let ex3 = Letfun("deep", "x", 
-                 If(Prim("=", Var "x", CstI 0),
-                    CstI 1,
-                    Call(Var "deep", Prim("-", Var "x", CstI 1))),
-                 Call(Var "deep", Var "count"));;
-    
-let rundeep n = eval ex3 [("count", Int n)];;
-
-(* Example: static scope (result 14) or dynamic scope (result 25) *)
-
-let ex4 =
-    Let("y", CstI 11,
-        Letfun("f", "x", Prim("+", Var "x", Var "y"),
-               Let("y", CstI 22, Call(Var "f", CstI 3))));;
-
-(* Example: two function definitions: a comparison and Fibonacci *)
-
-let ex5 = 
-    Letfun("ge2", "x", Prim("<", CstI 1, Var "x"),
-           Letfun("fib", "n",
-                  If(Call(Var "ge2", Var "n"),
-                     Prim("+",
-                          Call(Var "fib", Prim("-", Var "n", CstI 1)),
-                          Call(Var "fib", Prim("-", Var "n", CstI 2))),
-                     CstI 1), Call(Var "fib", CstI 25)));;
-
+(* Examples are now in FunOld.txt *)
 
 ////////////////////////
 //                    //
